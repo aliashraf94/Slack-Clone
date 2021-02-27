742dccf7cc48efd5a2b84b849e4a95f263441f60
Migracode <migracode@192.168.10.3>
2021-02-28 02:19:04 +0500

    mockup

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react"
+import './App.css';
+
+function App() {
+  return (
+   <div className="App">
+     <h1>Lets build Slack Clone </h1>
+
+     {/* Header */}
+     {/*Sidebar*/}
+     {/*React-Router for switching between channels*/}
+
+   </div>
+  );
+}
+
+export default App;
---
+++ src/Header.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./Header.css";
+
+function Header() {
+    return(
+        <div className="header">
+            
+            <div className="header_left" >
+                {/* Avatar for logged in user */}
+                {/* Time icon */}
+            </div>
+            <div className="header_search" >
+                {/* Search icon */}
+                {/* Input */}
+            </div>
+            <div className="header_right" >
+                {/* help icon*/}
+            </div>
+
+        </div>
+    );
+};
+
+
+
+export default Header;
