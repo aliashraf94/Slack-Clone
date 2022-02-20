f681284112824eff7265bffb0881acf7c4bde5be
andresgutgon <andresgutgon@gmail.com>
2022-02-20 10:31:23 +0100

    Fix Sidebar getting channels by using the right API from Firebase The code was using firestore/lite. I don't know if it was intentional and I don't have much experience with Firebase but I just fixed it by using firestore normal collection as explained in the documentation. Also check that the way the "useEffect" was implemented wasn't right. When doing something async in a "useEffect" you have to do an async function inside the useEffect for example and call that function.

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -11,7 +11,7 @@ import AppsIcon from "@material-ui/icons/Apps";
 import ExpandLessIcon from "@material-ui/icons/ExpandLess";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import AddIcon from "@material-ui/icons/Add"
-import {collection , getDocs , doc} from "firebase/firestore/lite"
+import { collection, getDocs } from "firebase/firestore"
 
 import SidebarOptions from "./SidebarOptions"
 import db from "./firebase"
@@ -20,29 +20,20 @@ import "./Sidebar.css"
 
 function Sidebar() {
   const [channels, setChannels] = useState([])
-  // const roomsCol = collection( db, "rooms");
-  // const snapShot = getDocs(roomsCol);
-  console.log(db)
+  useEffect(()=>{
+    async function fetchChannels() {
+      const snap = await getDocs(collection(db, "rooms"))
+      const channelsCollection = []
+      snap.forEach((doc) => {
+        channelsCollection.push({
+          id: doc.id,
+          name: doc.data().name
+        })
+      })
+      setChannels(channelsCollection)
+    }
 
-  // const docRef = doc(db, "rooms");
-  // const docSnap = getDoc(docRef);
-
-  // if (docSnap.exists()) {
-  // console.log("Document data:", docSnap.data());
-  // } else {
-  // // doc.data() will be undefined in this case
-  // console.log("No such document!");
-  // }
-
-  useEffect(() => {
-    const roomsCol = collection(db, 'rooms');
-    const roomsSnapShot = getDocs(roomsCol);
-    setChannels(
-      roomsSnapShot.getDoc.map((doc)=>({
-        id: doc.id,
-        name: doc.data().name
-      }))
-    )
+    fetchChannels()
   },[])
   return (
     <div className="sidebar">
@@ -72,7 +63,13 @@ function Sidebar() {
         <hr className="break-line" ></hr>
         <SidebarOptions Icon={AddIcon} title="Add Channels" />
         {channels.map((channel) => {
-          <SidebarOptions title={channel.name} id={channel.id} />
+          return (
+            <SidebarOptions
+              key={channel.id}
+              title={channel.name}
+              id={channel.id}
+            />
+          )
         })}
       </div>
     </div>
